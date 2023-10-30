07e1c9f13a4a2854a25f8839d2ccd858b90ff69b
Priyankareddy-nalla <148527465+Priyankareddy-nalla@users.noreply.github.com>
2023-10-30 22:40:10 +0100

    upadte scripts.js
    pokemonList  added to scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
-// alert 
-alert('Hello World');
 
-//variable declaration
-let favoriteFood = 'Chicken Biryani';
-document.write(favoriteFood);
+// Array list for Pokemon
+let pokemonList = [
+    { name: "Bulbasaur", height: 5, types: ['grass', 'poison'] },
+    { name: "Ivysaur", height: 4, types: ['grass', 'poison'] },
+    { name: "Venusaur", height: 7, types: ['grass', 'poison'] },
+    { name: "Charmander", height: 3, types: ['grass', 'fire'] }];
