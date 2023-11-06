97dee22aa987c8ebc5e53f6371e3b89945ddf1f5
Priyankareddy-nalla <148527465+Priyankareddy-nalla@users.noreply.github.com>
2023-11-06 21:38:05 +0100

    Update scripts.js
    applying forEach loop with conditions

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,27 @@ let pokemonList = [
 
 //forEach loop 
 
+// pokemonList.forEach(function (pokemon) {
+//     document.write("<br>",'My name is '  +pokemon.name + '  height ' + pokemon.height + ' types  ' + pokemon.types);
+// });
+
+//forEach loop with condtions
 pokemonList.forEach(function (pokemon) {
-    document.write("<br>",'My name is '  +pokemon.name + '  height ' + pokemon.height + ' types  ' + pokemon.types);
+    //here it checks pokemon height <6 and >4 then answer is 5
+    if (pokemon.height < 6 && pokemon.height > 4) {
+        document.write(pokemon.name + '(height: ' + pokemon.height + ')');
+        document.write("<br>");
+        // it checks equal to 7 
+    } else if (pokemon.height === 7) {
+        document.write(pokemon.name + '(height: ' + pokemon.height + ')'
+            + "- Wow, that\'s big!");
+        document.write("<br>");
+        //remaining pokemon list displayed
+    } else {
+        document.write(pokemon.name + '(height: ' + pokemon.height + ')');
+        document.write("<br>");
+
+    }
 });
 
 // Javascript body color 
