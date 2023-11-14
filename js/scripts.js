446e761c78c500fa605623bbccbca9c5fb9a530e
Priyankareddy-nalla <148527465+Priyankareddy-nalla@users.noreply.github.com>
2023-11-14 22:36:27 +0100

    Update scripts.js
    pokemonList count changed

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 //pokemon List
 let pokemonRepository = (function () {
     let pokemonList = [];
-    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/';
 
     function add(pokemon) {
         if (
